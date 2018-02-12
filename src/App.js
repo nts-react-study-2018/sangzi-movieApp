9357b7c3f20e4e2ca7da6197c9f19601ac5d820f
이상진 <sangzin.lee@nhn.com>
2018-02-12 09:47:47 +0900

    [MovieApp] ~ Updating Movie

--- src/App.js
+++ src/App.js
@@ -3,54 +3,53 @@ import './App.css';
 import Movie from './Movie.js';
 
 class App extends Component {
-  state = { }
+  state ={}
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Lalaland",
-            poster: "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/2azT/image/qUYSjM4PWnEbPj_kg-CA6BLm6i8.png"
-          },
-          {
-            title: "Lion King",
-            poster: "https://studybreaks.com/wp-content/uploads/2017/08/the-lion-king-image.jpg"
-          },
-          {
-            title: "Minions",
-            poster: "http://cfile2.uf.tistory.com/image/277DA137563865442830CB"
-          },
-          {
-            title: "About Time",
-            poster: "http://cfile26.uf.tistory.com/image/236A5E4D52D6B15B03FD29"
-          },
-          {
-            title: "Harry Potter",
-            poster: "https://resizing.flixster.com/YYdNO_mQd8hXa_qekp7cWlAnyII=/180x240/v1.bTsxMTMwNDIyNjtqOzE3NjgwOzEyMDA7MTUyMDsyMTUx"
-          }
-        ]
-      })
-    }, 1500);
+    this._getMovies();
+  
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map(function(arr, index){
-      return <Movie title={arr.title} poster={arr.poster} key={index}/>
+    const movies = this.state.movies.map(function(arr){
+      // console.log(arr)
+      // console.log( arr.genre_ids )
+      
+      return <Movie 
+        title={arr.original_title} 
+        poster={"https://image.tmdb.org/t/p/w500" + arr.poster_path} 
+        key={arr.id}
+        synopsis={arr.overview}
+        genres={arr.genre_ids}
+      />
     }) 
     return movies;
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies: movies
+    })
+  }
+
+  // api_key=fedb207b7efaaf0fdfaf8a48419c4fe6&language=en
+  // https://api.themoviedb.org/3/genre/movie/list?api_key=fedb207b7efaaf0fdfaf8a48419c4fe6&language=en-US
+
+  _callApi = () => {
+    return fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=fedb207b7efaaf0fdfaf8a48419c4fe6&language=en&page=2')
+    .then(patato => patato.json())
+    .then(json => json.results )
+    .catch(err => console.log(err))
   }
 
   render(){
     return (
       <div>
-        { 
-          this.state.movies ? this._renderMovies() : "loading"
-        }
+        { this.state.movies ? this._renderMovies() : "loading" }
       </div>
     );
   }
-
 }
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -2,28 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css'
 
-function Movie({title, poster}){
+function Movie({title, poster, synopsis, genres}){
     return (
-        <div>
-            <h1>{title}</h1>
-            <MoviePoster poster={poster} />
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title} title={title} />
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {
+                        genres.map((genre, index) => 
+                            <MovieGenres genre={genre} key={index} />
+                        )
+                    }
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    synopsis: PropTypes.string.isRequired,
+    genre_ids: PropTypes.array
 }
 
-function MoviePoster({poster}){
+function MoviePoster({poster, title}){
     return (
-        <img src={poster} alt="" />
+        <img src={poster} alt={title} title={title} className="Movie__Poster" />
     )
 }
 
 MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+}
+
+function MovieGenres({genre}){
+    return (
+        <span className="Movie__Genre">{genre} </span> 
+    )
 }
 
 export default Movie;
